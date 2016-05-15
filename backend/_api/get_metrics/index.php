1acba4026ac223f994a16575f0e651a7b877f85a
Dmitry Shmelev <avikez@gmail.com>
2016-05-15 14:11:25 +0300

    move get_metrics methods to helpers file

--- backend/_api/get_metrics/index.php
+++ backend/_api/get_metrics/index.php
@@ -1,5 +1,6 @@
 <?php
 include '../settings.php';
+include 'helpers.php';
 
 $num = $_GET['num'];
 $col = $_GET['col'];
@@ -36,28 +37,4 @@ mysql_close($link);
 
 echo_to_json( array_reverse( reduce_array($output, $col) ) );
 
-function reduce_array($array, $new_len) {
-    $len = count($array);
-      if ($len < $new_len) {
-            return $array;
-              }
-      $seperation = $len / $new_len;
-      $assoc = array(); // stores result
-        for($i = 0; $i < count($array)-1; $i = $i+$seperation ) {
-              $calc_value = $array[ceil($i)];
-                  array_push($assoc, $calc_value);
-                }
-        return $assoc;
-}
-
-function fails_with_json($str, $link){
-    defined($link) or mysql_close($link);
-      $msg['error'] = $str;
-      echo_to_json($msg);
-        die;
-}
-
-function echo_to_json($value) {
-    echo json_encode($value);
-}
 ?>
