d228539be17ecfa074335797d1ac53a09bc76f90
Dmitry Shmelev <avikez@gmail.com>
2016-10-12 17:36:14 +0300

    Refactor backend; Add DB stats metrics

--- backend/_api/get_metrics/helpers.php
+++ backend/_api/get_metrics/helpers.php
@@ -1,28 +1,28 @@
 <?php
 
 function reduce_array($array, $new_len) {
-    $len = count($array);
-      if ($len < $new_len) {
-            return $array;
-              }
-      $seperation = $len / $new_len;
-      $assoc = array(); // stores result
-        for($i = 0; $i < count($array)-1; $i = $i+$seperation ) {
-              $calc_value = $array[ceil($i)];
-                  array_push($assoc, $calc_value);
-                }
-        return $assoc;
+  $len = count($array);
+  if ($len < $new_len) {
+    return $array;
+  }
+  $seperation = $len / $new_len;
+  $assoc = array(); // stores result
+  for($i = 0; $i < count($array)-1; $i = $i+$seperation ) {
+    $calc_value = $array[ceil($i)];
+    array_push($assoc, $calc_value);
+  }
+  return $assoc;
 }
 
-function fails_with_json($str, $link){
-    defined($link) or mysql_close($link);
-      $msg['error'] = $str;
-      echo_to_json($msg);
-        die;
+function fails_with_json($str, $link = null){
+  is_null($link) ?: mysql_close($link);
+  $msg['error'] = $str;
+  echo_to_json($msg);
+  die;
 }
 
 function echo_to_json($value) {
-    echo json_encode($value);
+  echo json_encode($value);
 }
 
 ?>
--- backend/_api/get_metrics/index.php
+++ backend/_api/get_metrics/index.php
@@ -9,20 +9,20 @@ include 'helpers.php';
 # Constants
 $MULTIPLICATOR = 1.53;
 
-$num = $_GET['num'];
-$col = $_GET['col'];
-isset($col) OR $col = 15;
-
-if (!is_numeric($num)) {
-    fails_with_json("num is not numeric!");
-}
-if (!is_numeric($col)) {
-    fails_with_json("col is not numeric!");
-}
+# Variables
+$num = ( isset($_GET['num']) ? $_GET['num'] : null );
+$col = ( isset($_GET['col']) ? $_GET['col'] : 15   );
+is_numeric($num) ?: fails_with_json("num is not numeric!");
+is_numeric($col) ?: fails_with_json("col is not numeric!");
 if ($num < $col) {
     $num = $col;
 }
 
+$output = [[1476277202000,182],[1476277503000,120],[1476277803000,111],[1476278104000,108]];
+echo_to_json( array_reverse( reduce_array($output, $col) ) );
+exit;
+
+# Logics
 $link = mysql_connect($MYSQL_HOST, $MYSQL_USER, $MYSQL_PASS)
     or fails_with_json( 'mysql_connect: ' . mysql_error() );
 
@@ -48,4 +48,5 @@ echo_to_json( array_reverse( reduce_array($output, $col) ) );
 $time_end = microtime(true);
 $execution_time = ($time_end - $time_start) * 1000;
 header('API-Response-Time: ' . round($execution_time,2) . 'ms');
+header('API-QStats-count: ' . count($output));
 ?>
