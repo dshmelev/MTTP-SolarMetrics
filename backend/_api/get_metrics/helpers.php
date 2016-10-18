f1031f6b3014b3ff3cd57227ef0251b46672874d
Dmitry Shmelev <avikez@gmail.com>
2016-10-18 09:47:11 +0300

    Reduce to mysql

--- backend/_api/get_metrics/helpers.php
+++ backend/_api/get_metrics/helpers.php
@@ -1,19 +1,5 @@
 <?php
 
-function reduce_array($array, $new_len) {
-  $len = count($array);
-  if ($len <= $new_len) {
-    return $array;
-  }
-  $seperation = $len / $new_len;
-  $assoc = array(); // stores result
-  for($i = 0; $i < count($array)-1; $i = $i+$seperation ) {
-    $calc_value = $array[ceil($i)];
-    array_push($assoc, $calc_value);
-  }
-  return $assoc;
-}
-
 function fails_with_json($str, $link = null){
   is_null($link) ?: mysql_close($link);
   $msg['error'] = $str;
--- backend/_api/get_metrics/index.php
+++ backend/_api/get_metrics/index.php
@@ -12,13 +12,13 @@ $MULTIPLICATOR = 1.53;
 # Variables
 $offset = ( isset($_GET['offset']) ? $_GET['offset'] : 'last' );
 if ($offset == 'last') {
-  $num = '1 DAY'; $col = 1;
+  $num = '1 DAY';   $group = 'hour';
 } elseif ($offset == 'day') {
-  $num = '1 DAY'; $col = 24;
+  $num = '1 DAY';   $group = 'hour';
 } elseif ($offset == 'month') {
-  $num='30 DAY'; $col=30;
+  $num = '1 MONTH';  $group = 'day';
 } elseif ($offset == 'year') {
-  $num='365 DAY'; $col=12;
+  $num = '1 YEAR'; $group = 'month';
 } else {
   fails_with_json("offset is not valid!");
 }
@@ -30,9 +30,10 @@ $link = mysql_connect($MYSQL_HOST, $MYSQL_USER, $MYSQL_PASS)
 mysql_select_db($MYSQL_DBASE, $link)
     or fails_with_json( 'mysql_select: ' . mysql_error(), $link );
 
-$query = sprintf('SELECT UNIX_TIMESTAMP(timestamp) as timestamp, value FROM %s WHERE (`timestamp` > DATE_SUB(now(), INTERVAL %s)) ORDER BY id DESC',
+$query = sprintf('SELECT UNIX_TIMESTAMP(timestamp) as timestamp, SUM(value) FROM %s WHERE (`timestamp` > DATE_SUB(now(), INTERVAL %s)) GROUP BY %s(timestamp) ORDER BY id DESC',
   $MYSQL_TABLE_METRICS,
-  mysql_real_escape_string($num));
+  mysql_real_escape_string($num),
+  mysql_real_escape_string($group));
 $result = mysql_query($query) or fails_with_json('mysql_query: ' . mysql_error());
 
 $output = [];
@@ -44,10 +45,9 @@ while ($r = mysql_fetch_row($result, MYSQL_ASSOC)) {
 mysql_free_result($result);
 mysql_close($link);
 
-echo_to_json( array_reverse( reduce_array($output, $col) ) );
+echo_to_json( array_reverse( $output ) );
 
 $time_end = microtime(true);
 $execution_time = ($time_end - $time_start) * 1000;
 header('API-Response-Time: ' . round($execution_time,2) . 'ms');
-header('API-QStats-count: ' . count($output));
 ?>
